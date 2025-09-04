12fc08516a0d176d591ba862f5da457d3cbce182
krishaladangol <krishaladangol16@gmail.com>
2025-09-04 09:22:55 +0545

    Registration

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Models\Chirp;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChirpController;
@@ -9,9 +10,21 @@ Route::get('/', function () {
 });
 
 
-Route::get('/chirps',[ChirpController::class,'index'])->name('chirps.index');
+// Route::get('/chirps',[ChirpController::class,'index'])->name('chirps.index');
+// Route::post('/chirps',[ChirpController::class,'store'])->name('chirps.store');
+// Route::get('/chirps/{id}/edit',[ChirpController::class,'edit'])->name('chirps.edit');
+// Route::put('/chirps/{id}', [ChirpController::class, 'update'])->name('chirps.update');
+
+// Route::delete('/chirps/{id}', [ChirpController::class, 'destroy'])->name('chirps.destroy');
+
+Route::middleware('auth')->group(function(){
+    Route::get('/chirps',[ChirpController::class,'index'])->name('chirps.index');
 Route::post('/chirps',[ChirpController::class,'store'])->name('chirps.store');
 Route::get('/chirps/{id}/edit',[ChirpController::class,'edit'])->name('chirps.edit');
 Route::put('/chirps/{id}', [ChirpController::class, 'update'])->name('chirps.update');
 
 Route::delete('/chirps/{id}', [ChirpController::class, 'destroy'])->name('chirps.destroy');
+});
+
+Route::get('/register',[AuthController::class,'showRegistrationForm'])->name('register.form');
+Route::post('/register',[AuthController::class,'register'])->name('register');
